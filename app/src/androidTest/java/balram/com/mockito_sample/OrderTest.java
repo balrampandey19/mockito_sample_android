4fddd309a2a8dac90c3c13ef7ab1cc97c4839c0a
Balram Pandey <balrampandey19@gmail.com>
2017-05-08 18:42:40 +0530

    initial commit

--- app/src/androidTest/java/balram/com/mockito_sample/OrderTest.java
+++ app/src/androidTest/java/balram/com/mockito_sample/OrderTest.java
@@ -10,12 +10,9 @@ public class OrderTest extends InstrumentationTestCase {
 
     public void testInStock() {
         Warehouse mockWarehouse = mock(Warehouse.class);
-
         when(mockWarehouse.hasInventory("Talisker", 50)).thenReturn(true);
-
         Order order = new Order("Talisker", 50);
         order.fill(mockWarehouse);
-
         assertTrue(order.isFilled());
         verify(mockWarehouse).remove("Talisker", 50);
     }
@@ -24,12 +21,9 @@ public class OrderTest extends InstrumentationTestCase {
 
     public void testOutOfStock() {
         Warehouse mockWarehouse = mock(Warehouse.class);
-
         when(mockWarehouse.hasInventory("Talisker", 50)).thenReturn(false);
-
         Order order = new Order("Talisker", 50);
         order.fill(mockWarehouse);
-
         assertFalse(order.isFilled());
     }
 }
